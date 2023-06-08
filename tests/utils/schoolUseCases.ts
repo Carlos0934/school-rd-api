07c29e7bc34c5a904c1052db750552cc1e5a69c4
Carlos Olivo <carlosengel011@gmail.com>
2023-06-08 16:11:21 -0400

    getSchoolUseCase tests added

--- tests/utils/schoolUseCases.ts
+++ tests/utils/schoolUseCases.ts
@@ -1,16 +1,27 @@
-import { InMemoryRepository } from '../../src/infrastructure/inMemoryRepository.ts'
-import { SchoolService } from '../../src/services/school.service.ts'
-import { School } from '../../src/types.ts'
-import { SearchSchoolUseCase } from '../../src/usesCases/searchSchool.useCase.ts'
+import { InMemoryRepository } from "../../src/infrastructure/inMemoryRepository.ts";
+import { SchoolService } from "../../src/services/school.service.ts";
+import { School } from "../../src/types.ts";
+import { GetSchoolUseCase } from "../../src/usesCases/getSchoolUseCase.ts";
+import { SearchSchoolUseCase } from "../../src/usesCases/searchSchool.useCase.ts";
 
 export class SchoolUseCaseBuilder {
-	static buildSearchSchoolUseCase(data: School[] = []) {
-		const repository = new InMemoryRepository(data)
+  static buildSearchSchoolUseCase(data: School[] = []) {
+    const repository = new InMemoryRepository(data);
 
-		const service = new SchoolService(repository)
+    const service = new SchoolService(repository);
 
-		const searchSchoolsUseCase = new SearchSchoolUseCase(service)
+    const searchSchoolsUseCase = new SearchSchoolUseCase(service);
 
-		return searchSchoolsUseCase
-	}
+    return searchSchoolsUseCase;
+  }
+
+  static buildGetSchoolUseCaseWithSchools(data: School[]) {
+    const repository = new InMemoryRepository(data);
+
+    const service = new SchoolService(repository);
+
+    const getSchoolUseCase = new GetSchoolUseCase(service);
+
+    return getSchoolUseCase;
+  }
 }
